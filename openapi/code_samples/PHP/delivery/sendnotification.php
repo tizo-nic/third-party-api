c092e59abb6c3e4ac3b236e664a7edee9a0aff83
pacisauctor <agarciadarce@gmail.com>
2022-07-14 16:41:10 -0600

    casi finalizado envios

---
+++ openapi/code_samples/PHP/delivery/sendnotification.php
@@ -0,0 +1,14 @@
+<?php
+$client = new Client();
+$headers = [
+  'Authorization' => 'eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoidmVuZG9yIiwidXNlcm5hbWUiOiJ2X2dhYnJpZWxfNiIsImlhdCI6MTY1NzgwOTQ2MSwiZXhwIjoxNjU4NDE0MjYxfQ.N_rPvlIFgpj_mK8mcWzDbKEgud6bJeSNc5cOKqzyYTvWeCqStlzUCQjeyoNt98GwqpEU-4jdI35jMk83bahzpg',
+  'Content-Type' => 'application/json'
+];
+$body = '{
+  "whatsapp": false,
+  "sms": false,
+  "email": true
+}';
+$request = new Request('PUT', 'https://api.tizo.co/api/v1/delivery/send-notification/98871c18-a766-4b06-b528-f39d0b2666ba/', $headers, $body);
+$res = $client->sendAsync($request)->wait();
+echo $res->getBody();
---
+++ openapi/code_samples/python/delivery/sendnotification.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+url = "https://api.tizo.co/api/v1/delivery/send-notification/98871c18-a766-4b06-b528-f39d0b2666ba/"
+
+payload = json.dumps({
+  "whatsapp": False,
+  "sms": False,
+  "email": True
+})
+headers = {
+  'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoidmVuZG9yIiwidXNlcm5hbWUiOiJ2X2dhYnJpZWxfNiIsImlhdCI6MTY1NzgwOTQ2MSwiZXhwIjoxNjU4NDE0MjYxfQ.N_rPvlIFgpj_mK8mcWzDbKEgud6bJeSNc5cOKqzyYTvWeCqStlzUCQjeyoNt98GwqpEU-4jdI35jMk83bahzpg',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("PUT", url, headers=headers, data=payload)
+
+print(response.text)
