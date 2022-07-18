052318e7bc634c43a18a698993aa757c04630209
renatalechado9 <renataelenalechadovanegas@gmail.com>
2022-07-18 12:33:35 -0600

    terminado data

---
+++ openapi/code_samples/PHP/data/liststatusdelivery.php
@@ -0,0 +1,8 @@
+<?php
+$client = new Client();
+$headers = [
+  'Authorization' => 'eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoidmVuZG9yIiwidXNlcm5hbWUiOiJ2X2F4ZWxfOSIsImlhdCI6MTY1ODE2NDM4OCwiZXhwIjoxNjU4NzY5MTg4fQ.vvLtYB4O73futNvwzTZt2LhFzNV--XvMfTT5-62YWtxX-9CY6X4-UtIVDkCR_MX4YqX83AcCXiquHAb8uNHIjw'
+];
+$request = new Request('GET', 'https://api.tizo.co/api/v1/system-data/status-deliveries/', $headers);
+$res = $client->sendAsync($request)->wait();
+echo $res->getBody();
